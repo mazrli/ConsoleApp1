da7e998a46a99073eedec48285801b9cca2b5c91
Mr Aziz <91855982+mazrli@users.noreply.github.com>
2022-03-12 22:19:47 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            singleton c = singleton.getInstance(); // det här bästa sätt att anväda 
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    sealed class singleton 
+    {
+        private static int counter;
+
+
+        private singleton()
+        {
+            counter += 1;
+            Console.WriteLine("Instance Number " + counter.ToString());
+
+        }
+
+
+        public static singleton getInstance()
+        {
+            return new singleton();
+
+        }
+
+    }
+}
